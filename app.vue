4ed0682e2a3f4ebb3f511e0cbf6fca3c61012c14
Donald Zou <donaldzou@live.hk>
2025-08-28 02:19:09 +0800

    Update app.vue

--- app.vue
+++ app.vue
@@ -78,7 +78,6 @@ onMounted(() => {
 						Access Demo
 					</a>
 				</div>
-				<GoogleAds width="100%" height="200px" class="mt-5" data-ad-slot="1927542722"></GoogleAds>
 			</div>
 			<hr>
 			<div class="container d-flex flex-column gap-4 blurIn">
